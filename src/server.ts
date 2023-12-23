acd4ddee9767b9109226a1859367389a269eee63
Owen Susanto <owen@susanto.me>
2023-12-23 15:13:34 +0700

    test running server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import app from './app';
 
-app.listen(process.env.APP_PORT, () => {
-  console.log(`Server is running on http://localhost:${process.env.APP_PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
